7256f529e6434bd3f1928a3929404c5044f407e2
yushulx <lingxiao1002@gmail.com>
2023-11-06 16:24:26 +0800

    Updated

--- index.js
+++ index.js
@@ -102,10 +102,12 @@ var DocRectifier = /** @class */ (function () {
         var _this = this;
         return _this.obj.setParameters(params);
     };
-    DocRectifier.prototype.detectFileAsync = function (filePath) {
+    DocRectifier.prototype.detectFileAsync = function () {
         var _this = this;
-        return new Promise(function (resolve, reject) {
-            _this.obj.detectFileAsync(filePath, function (err, result) {
+        var filename = arguments[0];
+        var callback = arguments[1];
+        const promise = new Promise((resolve, reject) => {
+            _this.obj.detectFileAsync(filename, function (err, result) {
                 setTimeout(function () {
                     if (err) {
                         reject(err);
@@ -116,10 +118,23 @@ var DocRectifier = /** @class */ (function () {
                 }, 0);
             });
         });
+
+        if (callback && typeof callback === 'function') {
+            promise
+                .then(result => callback(null, result))
+                .catch(err => callback(err));
+        } else {
+            return promise;
+        }
     };
-    DocRectifier.prototype.detectBufferAsync = function (buffer, width, height, stride) {
+    DocRectifier.prototype.detectBufferAsync = function () {
         var _this = this;
-        return new Promise(function (resolve, reject) {
+        var buffer = arguments[0];
+        var width = arguments[1];
+        var height = arguments[2];
+        var stride = arguments[3];
+        var callback = arguments[4];
+        const promise = new Promise((resolve, reject) => {
             _this.obj.detectBufferAsync(buffer, width, height, stride, function (err, result) {
                 setTimeout(function () {
                     if (err) {
@@ -131,10 +146,28 @@ var DocRectifier = /** @class */ (function () {
                 }, 0);
             });
         });
+
+        if (callback && typeof callback === 'function') {
+            promise
+                .then(result => callback(null, result))
+                .catch(err => callback(err));
+        } else {
+            return promise;
+        }
     };
-    DocRectifier.prototype.normalizeFileAsync = function (filePath, x1, y1, x2, y2, x3, y3, x4, y4) {
+    DocRectifier.prototype.normalizeFileAsync = function () {
         var _this = this;
-        return new Promise(function (resolve, reject) {
+        var filePath = arguments[0];
+        var x1 = arguments[1];
+        var y1 = arguments[2];
+        var x2 = arguments[3];
+        var y2 = arguments[4];
+        var x3 = arguments[5];
+        var y3 = arguments[6];
+        var x4 = arguments[7];
+        var y4 = arguments[8];
+        var callback = arguments[9];
+        const promise = new Promise((resolve, reject) => {
             _this.obj.normalizeFileAsync(filePath, x1, y1, x2, y2, x3, y3, x4, y4, function (err, result) {
                 setTimeout(function () {
                     if (err) {
@@ -146,10 +179,31 @@ var DocRectifier = /** @class */ (function () {
                 }, 0);
             });
         });
+
+        if (callback && typeof callback === 'function') {
+            promise
+                .then(result => callback(null, result))
+                .catch(err => callback(err));
+        } else {
+            return promise;
+        }
     };
-    DocRectifier.prototype.normalizeBufferAsync = function (buffer, width, height, stride, x1, y1, x2, y2, x3, y3, x4, y4) {
+    DocRectifier.prototype.normalizeBufferAsync = function () {
         var _this = this;
-        return new Promise(function (resolve, reject) {
+        var buffer = arguments[0];
+        var width = arguments[1];
+        var height = arguments[2];
+        var stride = arguments[3];
+        var x1 = arguments[4];
+        var y1 = arguments[5];
+        var x2 = arguments[6];
+        var y2 = arguments[7];
+        var x3 = arguments[8];
+        var y3 = arguments[9];
+        var x4 = arguments[10];
+        var y4 = arguments[11];
+        var callback = arguments[12];
+        const promise = new Promise((resolve, reject) => {
             _this.obj.normalizeBufferAsync(buffer, width, height, stride, x1, y1, x2, y2, x3, y3, x4, y4, function (err, result) {
                 setTimeout(function () {
                     if (err) {
@@ -161,6 +215,14 @@ var DocRectifier = /** @class */ (function () {
                 }, 0);
             });
         });
+
+        if (callback && typeof callback === 'function') {
+            promise
+                .then(result => callback(null, result))
+                .catch(err => callback(err));
+        } else {
+            return promise;
+        }
     };
     DocRectifier.Template = Template;
     return DocRectifier;
